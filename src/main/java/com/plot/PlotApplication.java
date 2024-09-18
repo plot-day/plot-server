63fe095f97d55143b10838928bbf79bbd47d267e
ung <wkdu4510@gmail.com>
2024-09-18 21:03:31 +0900

    Create Project

---
+++ src/main/java/com/plot/PlotApplication.java
@@ -0,0 +1,13 @@
+package com.plot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlotApplication.class, args);
+	}
+
+}
