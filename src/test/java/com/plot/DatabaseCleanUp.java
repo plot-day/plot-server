48f1904c57f6f11c2e786ab44eafcb1c2b32bae2
ung <wkdu4510@gmail.com>
2024-09-21 18:38:24 +0900

    test / plot / set test configuration #1

---
+++ src/test/java/com/plot/DatabaseCleanUp.java
@@ -0,0 +1,60 @@
+package com.plot;
+
+import com.google.common.base.CaseFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Table;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class DatabaseCleanUp implements InitializingBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private List<String> tableNames;
+
+    @Override
+    public void afterPropertiesSet() {
+        final Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        tableNames = entities.stream()
+                .filter(e -> isEntity(e) && hasTableAnnotation(e))
+                .map(e -> e.getJavaType().getAnnotation(Table.class).name())
+                .collect(Collectors.toList());
+
+        final List<String> entityNames = entities.stream()
+                .filter(e -> isEntity(e) && !hasTableAnnotation(e))
+                .map(e -> CaseFormat.UPPER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, e.getName()))
+                .toList();
+
+        tableNames.addAll(entityNames);
+    }
+
+    private boolean isEntity(EntityType<?> e) {
+        return null != e.getJavaType().getAnnotation(Entity.class);
+    }
+
+    private boolean hasTableAnnotation(EntityType<?> e) {
+        return null != e.getJavaType().getAnnotation(Table.class);
+    }
+
+    @Transactional
+    public void excute() {
+        entityManager.flush();
+        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0").executeUpdate();
+
+        for (final String tableName: tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " AUTO_INCREMENT = 1").executeUpdate();
+        }
+
+        entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1").executeUpdate();
+    }
+}
